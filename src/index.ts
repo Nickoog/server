591ec5c2d8e3ab9e0c3f8cb70717457170e40e1a
Nickoog <nicolas.hoog@gmail.com>
2019-12-12 17:04:17 -0500

    refactor controller

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import express, { Request, Response } from 'express'
 import { router } from './routes/loginRoutes'
 import bodyParser from 'body-parser'
 import cookieSession from 'cookie-session'
+import { router as contrallerRouter } from './controllers/decorators/controller'
+import './controllers/LoginController'
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieSession({ keys: ['qwertyqwerty'] }))
 app.use(router)
+app.use(contrallerRouter)
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
